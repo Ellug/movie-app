6775f52e26a95160fcab06f7af77c6a3fcede9f8
Ellug <auron975@nate.com>
2023-12-07 17:02:15 +0900

    Create Project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from "../core/heropy";
+
+export default new Store({
+  photo: 'https://heropy.blog/css/images/logo.png',
+  name: 'Ellug / LeeDukGi',
+  email: 'onyx204652@gmail.com',
+  blog: 'https://heropy.blog',
+  github: 'https://github.com/ParkYoungWoong',
+  repository: 'https://github.com/ParkYoungWoong/vanillasjs-movie-app'
+})
